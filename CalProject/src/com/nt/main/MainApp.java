4c252a7ecf6539243a2d64e3d6d213fc11eefc4a
subbu <smaripi@metanoiasolutions.net>
2019-08-22 17:15:49 +0530

    modified main class in CalProject

--- CalProject/src/com/nt/main/MainApp.java
+++ CalProject/src/com/nt/main/MainApp.java
@@ -1,10 +1,15 @@
 package com.nt.main;
 
+import com.nt.add.Addition;
+
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Addition addition=new Addition();
+		int result=addition.add(29,47);
+		System.out.println("Addition of 29 and 47 is "+result);
 	}
 
 }
